a01c676119abd1edda43af529d6739d6cde2b82b
Luis Barria <lbarria77@gmail.com>
2023-05-22 22:41:57 -0500

    Primer Commit

---
+++ src/seed/seed.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@nestjs/common';
+import { PokeResponse } from './interfaces/poke-response.interface';
+import { Model } from 'mongoose';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { InjectModel } from '@nestjs/mongoose';
+import { AxiosAdapter } from '../common/adapters/axios.adapter';
+
+@Injectable()
+export class SeedService {
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+    private readonly http: AxiosAdapter,
+  ) {}
+
+  async executeSeed() {
+    await this.pokemonModel.deleteMany({});
+
+    const data = await this.http.get<PokeResponse>(
+      'https://pokeapi.co/api/v2/pokemon?limit=650',
+    );
+
+    // const insertPromisesArray = [];  // insert Promises Array
+    const pokemonToInsert = [];
+
+    data.results.forEach(({ name, url }) => {
+      const segments = url.split('/');
+      const no: number = +segments[segments.length - 2];
+
+      // const pokemon = await this.pokemonModel.create({ name, no });  // Insertar normal poner await en foreach
+      // insertPromisesArray.push(this.pokemonModel.create({ name, no }));  // insert Promises Array
+      pokemonToInsert.push({ name, no });
+    });
+
+    // await Promise.all(insertPromisesArray); // insert Promises Array
+    await this.pokemonModel.insertMany(pokemonToInsert);
+
+    return 'SEED Executed';
+  }
+}
